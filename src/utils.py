683c2dfa6085cf2f3bd32871b4265f944d468505
Akash kikani <akashkikani.github@gmail.com>
2023-05-15 14:50:28 -0400

    'model_training'

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 import pickle
 import numpy as np 
 import pandas as pd
-
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
 from src.exception import CustomException
 from src.logger import logging
@@ -19,6 +19,7 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
 def evaluate_model(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
